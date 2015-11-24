bcf5d61fea7b00d5eb6db153e5f587b08e7770d6
sachbearbeiter <sachbearbeiter@86879.no-reply.drupal.org>
2015-11-24 13:09:49 +0100

    Issue #2609062 by sachbearbeiter: Viewfield against D8rc3: Fatal error: Declaration of Drupal\viewfield\Plugin\Field\FieldFormatter\ViewfieldDefaultFormatter::viewElements()

--- src/Plugin/Field/FieldFormatter/ViewfieldDefaultFormatter.php
+++ src/Plugin/Field/FieldFormatter/ViewfieldDefaultFormatter.php
@@ -28,7 +28,7 @@ class ViewfieldDefaultFormatter extends FormatterBase {
   /**
    * {@inheritdoc}
    */
-  public function viewElements(FieldItemListInterface $items) {
+  public function viewElements(FieldItemListInterface $items, $langcode) {
     $elements = array();
 
     foreach ($items as $delta => $item) {
