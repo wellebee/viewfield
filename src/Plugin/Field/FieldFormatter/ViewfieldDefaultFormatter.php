0a082dade6d669ceeb9c230c8a2cd80ca19e1a18
Marcus Bernal <marcus.bernal@metaltoad.com>
2016-10-10 16:41:07 -0700

    Simplified the rendering of the viewfield.
    Removed the module file and moved the view argument parsing to the ViewfieldDefaultFormatter class.
    
    Adding a theme element seemed excessive for a couple of HTML element classes, so `viewfield_formatter_default` and its twig file was removed.
    
    Recursion prevention could be handled in the ViewfieldDefaultFormatter::prepareView() method. (WIP, likely next commit).

--- src/Plugin/Field/FieldFormatter/ViewfieldDefaultFormatter.php
+++ src/Plugin/Field/FieldFormatter/ViewfieldDefaultFormatter.php
@@ -8,6 +8,7 @@
 
 namespace Drupal\viewfield\Plugin\Field\FieldFormatter;
 
+use Drupal\Core\Entity\EntityInterface;
 use Drupal\Core\Field\FormatterBase;
 use Drupal\Core\Field\FieldItemListInterface;
 use Drupal\views\Views;
@@ -32,22 +33,72 @@ class ViewfieldDefaultFormatter extends FormatterBase {
     $elements = array();
 
     foreach ($items as $delta => $item) {
+      /* @var $item \Drupal\Core\Field\FieldItemBase */
+      /* @var $entity \Drupal\Core\Entity\Entity */
       $entity = $item->getEntity();
       list($view_name, $view_display) = explode('|', $item->vname, 2);
       $view = Views::getView($view_name);
-      $elements[$delta] = array(
-        '#type' => 'viewfield',
-        '#view' => $view,
-        '#access' => $view && $view->access($view_display),
-        '#view_name' => $view_name,
-        '#view_display' => $view_display,
-        '#view_arguments' => $item->vargs,
-        '#entity_type' => $entity->getEntityTypeId(),
-        '#entity_id' => $entity->id(),
-        '#entity' => $entity,
-        '#theme' => 'viewfield_formatter_default',
-      );
+      $view_args = $this->getViewArgs($item->vargs, $entity);
+      // Build the view display's renderable array per item.
+      $elements[$delta] = $view->buildRenderable($view_display, $view_args);
+      $elements[$delta]['#access'] = $view && $view->access($view_display);
     }
     return $elements;
   }
+
+  /**
+   * Parse argument string into an array and perform token replacements.
+   *
+   * @param string $view_args
+   *   The argument string from the entity edit/add form.
+   *
+   * @param \Drupal\Core\Entity\EntityInterface $entity
+   *   The entity to which the field is attached.
+   *
+   * @return array
+   *   List of arguments ready to be passed to a rendering method for a view.
+   */
+  protected function getViewArgs($view_args, EntityInterface $entity) {
+    $args = array();
+    $token_data = array($entity->getEntityTypeId() => $entity);
+
+    if (!empty($view_args)) {
+      $pos = 0;
+      while ($pos < strlen($view_args)) {
+        $found = FALSE;
+        // If string starts with a quote, start after quote and get everything
+        // before next quote.
+        if (strpos($view_args, '"', $pos) === $pos) {
+          if (($quote = strpos($view_args, '"', ++$pos)) !== FALSE) {
+            // Skip pairs of quotes.
+            while (!(($ql = strspn($view_args, '"', $quote)) & 1)) {
+              $quote = strpos($view_args, '"', $quote + $ql);
+            }
+            $args[] = str_replace('""', '"', substr($view_args, $pos, $quote + $ql - $pos - 1));
+            $pos = $quote + $ql + 1;
+            $found = TRUE;
+          }
+        }
+        elseif (($comma = strpos($view_args, ',', $pos)) !== FALSE) {
+          // Otherwise, get everything before next comma.
+          $args[] = substr($view_args, $pos, $comma - $pos);
+          // Skip to after comma and repeat
+          $pos = $comma + 1;
+          $found = TRUE;
+        }
+        if (!$found) {
+          $args[] = substr($view_args, $pos);
+          $pos = strlen($view_args);
+        }
+      }
+
+      $token = \Drupal::token();
+      foreach ($args as $key => $value) {
+        $args[$key] = $token->replace($value, $token_data);
+      }
+    }
+
+    return $args;
+  }
+
 }
